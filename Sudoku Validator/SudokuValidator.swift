bb2ce02c4d77cae7b1494915736d55975f60c2e1
kamillamamatova <kamillamamatova.km@gmail.com>
2025-06-24 00:41:01 -0400

    Implemented core validation logic

---
+++ Sudoku Validator/SudokuValidator.swift
@@ -0,0 +1,8 @@
+//
+//  SudokuValidator.swift
+//  Sudoku Validator
+//
+//  Created by Kamilla Mamatova on 6/24/25.
+//
+
+import Foundation
---
+++ Sudoku ValidatorTests/Sudoku_ValidatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Sudoku_ValidatorTests.swift
+//  Sudoku ValidatorTests
+//
+//  Created by Kamilla Mamatova on 6/24/25.
+//
+
+import Testing
+
+struct Sudoku_ValidatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
