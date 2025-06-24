454490a33ce45ba2666956aef834b9f0497a19ba
kamillamamatova <kamillamamatova.km@gmail.com>
2025-06-24 00:41:01 -0400

    Initial Commit

---
+++ Sudoku Validator/Sudoku_ValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_ValidatorApp.swift
+//  Sudoku Validator
+//
+//  Created by Kamilla Mamatova on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_ValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sudoku ValidatorUITests/Sudoku_ValidatorUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sudoku_ValidatorUITestsLaunchTests.swift
+//  Sudoku ValidatorUITests
+//
+//  Created by Kamilla Mamatova on 6/24/25.
+//
+
+import XCTest
+
+final class Sudoku_ValidatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
