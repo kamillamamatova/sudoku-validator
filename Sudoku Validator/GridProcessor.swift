eafc31f28b8770768cdff423f7d60d56efa818bd
kamillamamatova <kamillamamatova.km@gmail.com>
2025-06-27 11:42:36 -0400

    Implemented a struct that bundles a number its location together (actually)

--- Sudoku Validator/GridProcessor.swift
+++ Sudoku Validator/GridProcessor.swift
@@ -1,8 +1,20 @@
-//
-//  GridProcessor.swift
-//  Sudoku Validator
-//
-//  Created by Kamilla Mamatova on 6/27/25.
-//
-
 import Foundation
+import Vision
+import UIKit
+
+// A helper struct that holds a recognized digit and its position
+struct RecognizedDigit{
+    let text: String
+    let boundingBox: CGRect
+}
+
+class GridProcessor{
+    // Takes the raw observations from Vision and return a 9x9 grid
+    func process(observations: [VNRecognizedTextObservation]) -> [[Int]]{
+        // Future logic
+        
+        // Returns an empty board, for now
+        let emptyBoard = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+        return emptyBoard
+    }
+}
